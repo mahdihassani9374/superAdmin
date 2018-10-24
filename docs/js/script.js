a65b285446b061ff9ffa34450019052d4b797232
mahdihassani9374 <mahdihassani9374@gmail.com>
2018-10-24 09:24:18 +0330

    add scrollbar jquery

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,41 +1,40 @@
-$(function(){
+$(function () {
 
     new WOW().init();
 
     // start menu responsive
 
-    $('.header__menu').on('click',function(event){
+    $('.header__menu').on('click', function (event) {
         event.stopPropagation();
         $('.sidebar').addClass('toggled');
         $('body').addClass('aside-toggled');
     })
 
-    $('.sidebar').on('click',function(event){
+    $('.sidebar').on('click', function (event) {
         event.stopPropagation();
     })
 
-    $(window).click(function(event) {
+    $(window).click(function (event) {
         $('.sidebar').removeClass('toggled');
         $('body').removeClass('aside-toggled');
     });
 
     // end menu responsive
 
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 30) {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 30) {
             $('.header').addClass('header--scrolled');
-        }
-        else {
+        } else {
             $('.header').removeClass('header--scrolled');
         }
     })
 
     // show / hide sub menu
 
-    $('.sidebar__navigation__item').on('click',function(event){
-       event.stopPropagation();
-       //var active = $(this).find('.sidebar__navigation__item__link').addClass('active');
-       var subMenu = $(this).find('ul.sidebar__navigation-sub').slideToggle();
+    $('.sidebar__navigation__item').on('click', function (event) {
+        event.stopPropagation();
+        //var active = $(this).find('.sidebar__navigation__item__link').addClass('active');
+        var subMenu = $(this).find('ul.sidebar__navigation-sub').slideToggle();
     })
 
     // show / hide sub menu
@@ -43,14 +42,21 @@ $(function(){
     // scroll bar 
 
     $(".scroll-panel").mCustomScrollbar({
-        scrollButtons:{enable:true,scrollType:"stepped"},
-        keyboard:{scrollType:"stepped"},
-        mouseWheel:{scrollAmount:0,normalizeDelta:true},
-        theme:"rounded-dark",
-        autoExpandScrollbar:true,
-        snapAmount:188,
-        snapOffset:65
-    });
+        scrollButtons: {
+            enable: true,
+            scrollType: "stepped"
+        },
+        keyboard: {
+            scrollType: "stepped"
+        },
+        mouseWheel: {
+            scrollAmount: 188,
+            normalizeDelta: true
+        },
+        theme: "rounded-dark",
+        autoExpandScrollbar: true,
+        snapAmount: 188,
+        snapOffset: 65
 
-    
+    });
 })
